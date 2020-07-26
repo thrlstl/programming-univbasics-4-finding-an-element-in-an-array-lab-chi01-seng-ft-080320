1d0f34bef7a096245f42b43a5af1b2a7bfe2ea0e
Matthew Steele <matthewsteeleonline@gmail.com>
2020-07-26 23:44:27 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count = 0 
   found_value_index = nil
   while count < array.length do
-    puts array[count] == value_to_find
+    if array[count] == value_to_find
     count += log10
   end
 end
